350b32c2465cd4394a6d879782079b916c1a30dd
Voroshilov Max <voroshilovmax90@gmail.com>
2020-02-12 13:22:21 +0700

    finish with user registration

---
+++ config/path.js
@@ -0,0 +1,5 @@
+const path = "http://localhost:8080/fdb/demo/quize"
+
+module.exports = {
+    path
+}
--- services/users.js
+++ services/users.js
@@ -1,74 +1,66 @@
 
-const MongoClient = require('mongodb').MongoClient;
-const keys = require("../key");
+const axios = require("axios");
+const path = require("../config/path");
 
-const uri = keys.mongoKey;
-const fromDB = "Quize";
 
 const registration = (req, res) => {
-    MongoClient.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, function (err, db) {
-        if (err) {
+
+    let findNickName = {
+        "select": ["*"],
+        "from": ["users/nickName", req.body.nickName]
+    }
+
+    axios.post(path.path + "/query", findNickName).then((x) => {
+        if (x.data === null) {
+            let data = [{
+                "_id": "users$newUser",
+                "nickName": req.body.nickName,
+                "email": req.body.email,
+                "wallet": req.body.wallet,
+                "avatar": req.body.avatar
+            }]
+
+            axios.post(path.path + "/transact", data).then((x) => {
+                res.status(200).end();
+            }).catch((err) => {
+                res.status(400);
+                res.send("error to send new user to DB");
+            })
+
+        } else {
             res.status(400);
-            res.send("error database connection");
-            console.log("DB error: " + err)
+            res.send("user already exist");
         }
-        let dbo = db.db(fromDB);
-        dbo.collection("users").findOne({ nickName: req.body.nickName }, (err, result) => {
-            if (err) {
-                res.status(400);
-                res.send("error database connection");
-                console.log("DB error: " + err)
-            }
-            if (result === null) {
-                let data = {
-                    nickName: req.body.nickName,
-                    email: req.body.email,
-                    wallet: req.body.wallet,
-                    listHostEvents: [],
-                    listParticipantEvents: [],
-                    listValidatorEvents: [],
-                    avatar: req.body.avatar
-                }
-                dbo.collection("users").insertOne(data, function (err, response) {
-                    if (err) {
-                        res.status(400);
-                        res.send("error database connection");
-                        console.log("DB error: " + err)
-                    }
-                    res.status(200).end();
-                    db.close();
-                });
-            } else {
-                res.status(400);
-                res.send("user already exist");
-                db.close();
-            }
-        })
-    });
+
+    }).catch((error) => {
+        console.log("findNickName error:" + error);
+        res.status(400);
+        res.send(error);
+    })
 }
 
 const validate = (req, res) => {
-    MongoClient.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, function (err, db) {
-        if (err) {
-            res.status(400);
-            res.send("error database connection");
-            console.log("DB error: " + err)
+    if (req.body.wallet !== undefined) {
+        let conf = {
+            "select": ["*"],
+            "from": ["users/wallet", req.body.wallet]
         }
-        let dbo = db.db(fromDB);
 
-        dbo.collection("users").findOne({ wallet: req.body.wallet }, (err, result) => {
-            if (err) {
-                res.status(400);
-                res.send("error database connection");
-                console.log("DB error: " + err)
-            }
-            if (result === null) {
+        axios.post(path.path + "/query", conf).then((x) => {
+
+            if (x.data !== null) {
+                let o = {
+                    _id: x.data["_id"],
+                    wallet: x.data["users/wallet"],
+                    nickName: x.data["users/nickName"],
+                    avatar: x.data["users/avatar"],
+                    email: x.data["users/email"]
+                }
+
+                res.status(200);
+                res.send(o);
+
+            } else {
                 let data = {
                     nickName: undefined,
                     email: undefined,
@@ -77,42 +69,34 @@ const validate = (req, res) => {
 
                 res.status(200);
                 res.send(data);
-                db.close();
-
-            } else {
-                res.status(200);
-                res.send(result);
-                db.close();
             }
+        }).catch((error) => {
+            console.log(error);
+            res.status(400);
+            res.send(error);
         })
-    });
+    } else {
+        res.status(400);
+        res.send("reqest is undefined");
+    }
 }
 
 const allUsers = (req, res) => {
-    MongoClient.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, function (err, db) {
-        if (err) {
-            res.status(400);
-            res.send("error database connection");
-            console.log("DB error: " + err)
-        }
-        let dbo = db.db(fromDB);
 
-        dbo.collection("users").find({}).toArray((err, result) => {
-            if (err) {
-                res.status(400);
-                res.send("error database connection");
-                console.log("DB error: " + err)
-            }
+    let conf = {
+        "select": ["*"],
+        "from": "users"
+    }
 
-            res.status(200);
-            res.send(result);
-            db.close();
-
-        })
-    });
+    axios.post(path.path + "/query", conf).then((x) => {
+        console.log(x.data);
+        res.status(200);
+        res.send(x.data);
+    }).catch((error) => {
+        console.log(error);
+        res.status(400);
+        res.send(error);
+    })
 }
 
 module.exports = {
