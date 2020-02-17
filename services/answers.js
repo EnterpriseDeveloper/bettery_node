03411fbffb79f2f053569fe14dea705daced9c07
Voroshilov Max <voroshilovmax90@gmail.com>
2020-02-17 19:22:14 +0700

    added history query

--- services/answers.js
+++ services/answers.js
@@ -39,10 +39,23 @@ const setOneAnswer = (req, res) => {
     }
     setAnswer.push(event)
 
-   // add to users table
+    // add history transaction
+    let history = {
+        _id: "historyTransactions$newHistory",
+        eventId: eventId,
+        role: req.body.from,
+        amount: req.body.money,
+        paymentWay: "send",
+        date: Math.floor(Date.now() / 1000)
+    }
+
+    setAnswer.push(history)
+
+    // add to users table
     let user = {
         _id: from,
-        activites: ["activites$act1"]
+        activites: ["activites$act1"],
+        historyTransactions: ["historyTransactions$newHistory"]
     }
     setAnswer.push(user)
 
