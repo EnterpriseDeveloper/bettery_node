e6f3449152f207627a9d427d6609ed2f8ed786bc
Voroshilov Max <voroshilovmax90@gmail.com>
2020-06-08 16:26:14 +0300

    done

--- config/path.js
+++ config/path.js
@@ -1,4 +1,6 @@
-const path = "http://flureedb:8080/fdb/demo/quize"
+const path = "http://54.255.229.79:8080/fdb/demo/quize"
+
+// const path = "http://flureedb:8080/fdb/demo/quize"
 
 module.exports = {
     path
