382833e5fe6a97060080a86a7bf77637a0e6e500
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-02-05 15:01:09 +0200

    added build for prodaction

--- config/path.js
+++ config/path.js
@@ -1,6 +1,5 @@
-const path = "http://54.255.205.9:8080/fdb/demo/quize"
-
-//const path = "http://flureedb:8080/fdb/demo/quize"
+let ip = process.env.NODE_ENV == "production" ? "13.212.36.199:8080" : "54.255.205.9:8080";
+const path = `http://${ip}/fdb/demo/quize`;
 
 module.exports = {
     path
