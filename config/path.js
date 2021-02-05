c9a28c448e71cb7d6bc59df6197c886e1500e4e1
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-02-05 16:21:28 +0200

    incorrect IP

--- config/path.js
+++ config/path.js
@@ -1,4 +1,4 @@
-let ip = process.env.NODE_ENV == "production" ? "13.212.36.199:8080" : "54.255.205.9:8080";
+let ip = process.env.NODE_ENV == "production" ? "13.212.2.84:8080" : "54.255.205.9:8080";
 const path = `http://${ip}/fdb/demo/quize`;
 
 module.exports = {
