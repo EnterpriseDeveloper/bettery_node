84565286df4691808543540228ee517f2788354d
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-03-29 18:18:22 +0300

    added git ignore

--- config/key.js
+++ config/key.js
@@ -1,5 +1,7 @@
-const apiKey = 'b615c4243192f32db11c07ef29a4e5eda4b9ab7924c9066148adb64c04bd9c6a'
+const apiKey = 'b615c4243192f32db11c07ef29a4e5eda4b9ab7924c9066148adb64c04bd9c6a';
+const infura = '2b5ec85db4a74c8d8ed304ff2398690d';
 
 module.exports = {
-   apiKey
+   apiKey,
+   infura
 }
--- config/networks.js
+++ config/networks.js
@@ -1,11 +1,13 @@
-const goerli = "https://goerli.infura.io/v3/2b5ec85db4a74c8d8ed304ff2398690d";
+const keys = require("./key");
+
+const goerli = `https://goerli.infura.io/v3/${keys.infura}`;
 const maticMumbaiWSS = 'wss://ws-mumbai.matic.today';
 const matciMainWSS = "wss://ws-mainnet.matic.network"
 const maticMumbaiHttps = 'https://rpc-mumbai.matic.today';
 const gasEstimationAPI = 'https://gasstation-mumbai.matic.today';
 const mainId = 5;
 const maticMumbaiId = 80001;
-const mainnet = "https://mainnet.infura.io/v3/2b5ec85db4a74c8d8ed304ff2398690d";
+const mainnet = `https://mainnet.infura.io/v3/${keys.infura}`;
 const mainnetID = 1;
 const maticMain = "https://rpc-mainnet.matic.network";
 const maticMainId = 137;
