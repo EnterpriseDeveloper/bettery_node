2db8324bd9974f7444b2b7003fca4f4e37227b10
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-03-31 15:25:23 +0300

    boom

--- contract-services/publicEvents/payToRefferers.js
+++ contract-services/publicEvents/payToRefferers.js
@@ -6,24 +6,34 @@ const payToRefferers = async (data) => {
     let id = data.id;
 
     // TODO get fake address from contract
-    let addr1 = [];
-    let addr2 = [];
-    let addr3 = [];
+    // let addr1 = [];
+    // let addr2 = [];
+    // let addr3 = [];
 
     // TODO add prodaction 
     let path = "test" // process.env.NODE_ENV
     let contract = await ContractInit.init(path, PlayerPaymentContract);
     try {
-        let gasEstimate = await contract.methods.payToReff(id, addr1, addr2, addr3).estimateGas();
-        await contract.methods.payToReff(id, addr1, addr2, addr3).send({
+        let gasEstimate = await contract.methods.payRefToComp(id, 1).estimateGas();
+        await contract.methods.payRefToComp(id, 1).send({
             gas: gasEstimate,
             gasPrice: 0
         });
 
-        // TODO add to db event finish
     } catch (err) {
         console.log("err from pay to pay to refferers", err)
     }
+    // try {
+    //     let gasEstimate = await contract.methods.payToReff(id, addr1, addr2, addr3).estimateGas();
+    //     await contract.methods.payToReff(id, addr1, addr2, addr3).send({
+    //         gas: gasEstimate,
+    //         gasPrice: 0
+    //     });
+
+    //     // TODO add to db event finish
+    // } catch (err) {
+    //     console.log("err from pay to pay to refferers", err)
+    // }
 
 }
 
