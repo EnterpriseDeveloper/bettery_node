180b04e5e88c05c070fd1df4f174d3d047c4f774
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-04-02 16:13:53 +0300

    fixed typo

--- contract-services/publicEvents/payToExperts.js
+++ contract-services/publicEvents/payToExperts.js
@@ -1,7 +1,7 @@
 const MiddlePaymentContract = require("../abi/MiddlePayment.json");
 const ContractInit = require("../contractInit");
 const Web3 = require("web3");
-const urt = require("../../config/path");
+const url = require("../../config/path");
 
 const payToExperts = async (data) => {
     console.log("from payToExperts")
@@ -10,7 +10,10 @@ const payToExperts = async (data) => {
     let payHost = data.payHost;
     let mintAdv = data.mintAdv;
     let payAdv = data.payAdv;
-    let contract = await ContractInit.init(process.env.NODE_ENV, MiddlePaymentContract);
+
+    // TODO add prodaction 
+    let path = "test" // process.env.NODE_ENV
+    let contract = await ContractInit.init(path, MiddlePaymentContract);
     try {
         let gasEstimate = await contract.methods.letsPayToExperts(id).estimateGas();
         await contract.methods.letsPayToExperts(id).send({
@@ -19,8 +22,6 @@ const payToExperts = async (data) => {
         });
 
         await setToDb(mintHost, payHost, mintAdv, payAdv);
-
-        // TODO send to DB and calculate reputation of expert
     } catch (err) {
         console.log("err from pay to experts", err)
     }
@@ -29,11 +30,11 @@ const payToExperts = async (data) => {
 const setToDb = async (id, mintHost, payHost, mintAdv, payAdv) => {
     let web3 = new Web3();
     let data = [{
-      "_id": Number(id),
-      "mintedHostAmount": web3.utils.fromWei(String(mintHost), "ether"),
-      "payHostAmount": web3.utils.fromWei(String(payHost), "ether"),
-      "mintedAdvisorAmount": web3.utils.fromWei(String(mintAdv), "ether"),
-      "payAdvisorAmount": web3.utils.fromWei(String(payAdv), "ether")
+        "_id": Number(id),
+        "mintedHostAmount": web3.utils.fromWei(String(mintHost), "ether"),
+        "payHostAmount": web3.utils.fromWei(String(payHost), "ether"),
+        "mintedAdvisorAmount": web3.utils.fromWei(String(mintAdv), "ether"),
+        "payAdvisorAmount": web3.utils.fromWei(String(payAdv), "ether")
     }]
     await axios.post(`${url.path}/transact`, data).catch((err) => {
         console.log("DB error from transact payToLosers: " + err.response.data.message)
