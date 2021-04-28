096528bc1d267c168d81a0df01133348eb19a46e
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-04-28 11:42:08 +0300

    fixed isssue with keys on tokensale

--- contract-services/tokensale/index.js
+++ contract-services/tokensale/index.js
@@ -11,17 +11,20 @@ module.exports = app => {
         let from = req.body.from;
         let provider;
         let networkId;
+        let keys;
         if (from == "prod") {
             provider = config.mainnet;
-            networkId = config.mainnetID
+            networkId = config.mainnetID;
+            keys = "./keys/prod/privateKey";
         } else if (from == "dev") {
             provider = config.goerli;
             networkId = config.mainId;
+            keys = "./keys/test/privateKey";
         }
-        let tokenMarket = await tokenSale(provider, networkId);
+        let tokenMarket = await tokenSale(provider, networkId, keys);
         let tokenSold = await tokenMarket.methods.tokensSold().call();
         let price = await tokenMarket.methods.tokenPrice().call();
-        let betteryToken = await BetteryContract(provider, networkId);
+        let betteryToken = await BetteryContract(provider, networkId, keys);
         let balance = await betteryToken.methods.balanceOf(TokenSaleContract.networks[networkId].address).call();
         let web3 = new Web3();
         res.status(200);
@@ -34,23 +37,23 @@ module.exports = app => {
     })
 }
 
-async function BetteryContract(provider, networkId) {
-    let { web3, account } = await connectToContract(provider);
+async function BetteryContract(provider, networkId, keys) {
+    let { web3, account } = await connectToContract(provider, keys);
     let abi = BetteryTokenContract.abi;
     let address = BetteryTokenContract.networks[networkId].address;
     return new web3.eth.Contract(abi, address, { from: account });
 }
 
-async function tokenSale(provider, networkId) {
-    let { web3, account } = await connectToContract(provider);
+async function tokenSale(provider, networkId, keys) {
+    let { web3, account } = await connectToContract(provider, keys);
     let abi = TokenSaleContract.abi;
     let address = TokenSaleContract.networks[networkId].address;
     return new web3.eth.Contract(abi, address, { from: account });
 }
 
-async function connectToContract(provider) {
+async function connectToContract(provider, keys) {
     let web3 = new Web3(provider);
-    let privateKey = readFileSync(path.join(__dirname, '../privateKey'), 'utf-8')
+    let privateKey = readFileSync(path.join(__dirname, keys), 'utf-8')
     const prKey = web3.eth.accounts.privateKeyToAccount('0x' + privateKey);
     await web3.eth.accounts.wallet.add(prKey);
     let accounts = await web3.eth.accounts.wallet;
