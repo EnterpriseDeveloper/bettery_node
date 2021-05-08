54de29422f0372ad78022ab99edea5844cf01628
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-05-08 10:30:28 +0300

    changed prod provider https and wss links

--- config/key.js
+++ config/key.js
@@ -2,10 +2,12 @@ const apiKey = 'b615c4243192f32db11c07ef29a4e5eda4b9ab7924c9066148adb64c04bd9c6a
 const infura = '2b5ec85db4a74c8d8ed304ff2398690d';
 const APIprod = "18.139.161.170:8090";
 const APItest = "13.250.1.250:8090";
+const quiknode = "63e582ed6c17529d98cc064ba97cad1d95650da5"
 
 module.exports = {
    apiKey,
    infura,
    APIprod,
-   APItest
+   APItest,
+   quiknode
 }
--- config/networks.js
+++ config/networks.js
@@ -2,14 +2,14 @@ const keys = require("./key");
 
 const goerli = `https://goerli.infura.io/v3/${keys.infura}`;
 const maticMumbaiWSS = 'wss://ws-mumbai.matic.today';
-const matciMainWSS = "wss://ws-mainnet.matic.network"
+const matciMainWSS = `wss://wispy-spring-star.matic.quiknode.pro/${keys.quiknode}/`;
 const maticMumbaiHttps = 'https://rpc-mumbai.matic.today';
 const gasEstimationAPI = 'https://gasstation-mumbai.matic.today';
 const mainId = 5;
 const maticMumbaiId = 80001;
 const mainnet = `https://mainnet.infura.io/v3/${keys.infura}`;
 const mainnetID = 1;
-const maticMain = "https://rpc-mainnet.matic.network";
+const maticMain = `https://wispy-spring-star.matic.quiknode.pro/${keys.quiknode}/`;
 const maticMainId = 137;
 
 module.exports = {
