44b148c0e125790ce8cbecb14a58200f177e615e
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-05-12 11:34:20 +0300

    changed API

--- config/networks.js
+++ config/networks.js
@@ -1,15 +1,20 @@
 const keys = require("./key");
 
-const goerli = `https://goerli.infura.io/v3/${keys.infura}`;
-const maticMumbaiWSS = 'wss://ws-mumbai.matic.today';
+// Matic main
+const maticMain = `https://wispy-spring-star.matic.quiknode.pro/${keys.quiknode}/`;
 const matciMainWSS = `wss://wispy-spring-star.matic.quiknode.pro/${keys.quiknode}/`;
-const maticMumbaiHttps = 'https://rpc-mumbai.maticvigil.com/v1/0a4eb295e6abcd89e03e21b86b4e15a59fb1a8ab';
-const mainId = 5;
+const maticMainId = 137;
+// Mumbai
+const maticMumbaiHttps = 'https://matic-testnet-archive-rpc.bwarelabs.com';
+const maticMumbaiWSS = 'wss://ws-mumbai.matic.today';
 const maticMumbaiId = 80001;
+// Goerli
+const goerli = `https://goerli.infura.io/v3/${keys.infura}`;
+const mainId = 5;
+// Main Ether
 const mainnet = `https://mainnet.infura.io/v3/${keys.infura}`;
 const mainnetID = 1;
-const maticMain = `https://wispy-spring-star.matic.quiknode.pro/${keys.quiknode}/`;
-const maticMainId = 137;
+// Gas estimation
 const gasEstimationMumbaiAPI = 'https://gasstation-mumbai.matic.today';
 const gasEstimationMainAPI ="https://gasstation-mainnet.matic.network"
 
