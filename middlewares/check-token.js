026480b7501aa4639a49a51de3264c8eb6f8fde7
Andrii Sokhryn <asohrin9996@gmail.com>
2021-06-12 10:12:43 +0300

    added check-token middleware & redis-helper

---
+++ middlewares/check-token.js
@@ -0,0 +1,38 @@
+const crypto = require('crypto-js');
+const {getFromRedis} = require('../helpers/redis-helper')
+const {secretRedis} = require('../config/key')
+
+module.exports = async (req, res, next) => {
+    try {
+        const sessionToken = req.get('Authorization');
+        const accessToken = req.get('Cookies')
+
+        if (!sessionToken || !accessToken) {
+            return next(res.send('NO token'), 400)
+        }
+
+        const bytes  = crypto.AES.decrypt(sessionToken, secretRedis);
+        const decryptedData = bytes.toString(crypto.enc.Utf8);
+
+        const fromRedis = await getFromRedis(decryptedData)
+
+        if(!fromRedis) {
+            return next(res.send('NOT valid token'), 400)
+        }
+
+        const data = fromRedis.key.filter(el => {
+            return el.sessionKey === accessToken
+        });
+        if(!data.length) {
+            return next(res.send('NOT valid token'))
+        }
+        fromRedis.key = data
+        req.body.dataFromRedis = fromRedis;
+        next()
+    } catch (e) {
+        res.send(e.message);
+        next(e)
+    }
+}
+
+
