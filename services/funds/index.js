c3ef35ee7d6ae58f5a56a214ed9f904d8074dce8
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-06-29 21:02:08 +0300

    remove unused code

--- services/funds/index.js
+++ services/funds/index.js
@@ -1,13 +1,9 @@
-const setEthPrice = require("./ethPrice");
 const withdrawal = require("./withdrawal");
 const bettertToken = require('./betteryToken');
 const userTokens = require('./userTokens')
 const checkToken = require('../../middlewares/check-token');
 
 module.exports = app => {
-    app.get("/eth_price", async (req, res) => {
-        setEthPrice.getEthPrice(req, res);
-    })
 
     app.post("/withdrawal/init", async (req, res) => {
         withdrawal.setInitWithd(req, res);
