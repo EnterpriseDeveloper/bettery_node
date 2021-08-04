92c65e9ece5fc610ba0c36dd3264ac4b110e5896
ANDREW.KOZYAR <andrew.kozyar@gmail.com>
2021-08-04 15:43:50 +0300

    BETTERY-206 validate token by email nickNane date

--- middlewares/check-is-token-valid.ts
+++ middlewares/check-is-token-valid.ts
@@ -1,27 +1,34 @@
 import axios from "axios";
+import moment from "moment";
+
+import { auth0Path } from "../config/key"
 
 const checkIsTokenValid = async (req: any, res: any, next: any) => {
     try {
         const accessToken = req.body.accessToken
-        const loginPlatform = req.body.verifierId.split('|')
 
-        if(loginPlatform[0] === 'google-oauth2'){
-            await axios.get(`https://oauth2.googleapis.com/tokeninfo?access_token=${accessToken}`)
-            .then((res) => console.log(res.data))
-            .catch((err) => res.send(err).json('not valid token')) 
+        const checkToken = () => axios.get(`${auth0Path}/userinfo`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        .then((res) => res.data)
+        const tokenData = await checkToken()
 
+        if(tokenData.name == req.body.nickName || tokenData.email == req.body.email){
+            const datetime = tokenData.updated_at;
+            const localTime = moment();
+            const otherTime = moment(datetime);
+
+            if(localTime.diff(otherTime, 'minutes') <= 5){
+                next()
+            } else{
+                throw new Error(res.status(400).json('not valid token'))
+            }
+            
+        } else{
+            throw new Error(res.status(400).json('not valid token'))
         }
-
-
-        if(loginPlatform[0] === 'facebook'){
-            await axios.get(`https://graph.facebook.com/app?access_token=${accessToken}`)
-            .then((res) => console.log(res.data))
-            .catch((err) => res.send(err).json('not valid token'))
-
-        }
-        
-        
-        next()
     } catch (e) {
         res.send(e.message);
         next(e)
