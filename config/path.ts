c4076e49cf07dd310dd786e9742a8ba11fc4b9b5
Andrii <asohrin9996@gmail.com>
2021-08-25 10:51:00 +0300

    fixed data structure

--- config/path.ts
+++ config/path.ts
@@ -2,7 +2,7 @@ import { APIprod, APItest } from "./key";
 
 const ip = process.env.NODE_ENV == "production" ? APIprod : APItest;
 // const path = `http://${ip}/fdb/demo/quize`;
-const path = `http://localhost:8090/fdb/test/bettery2`;
+const path = `http://localhost:8090/fdb/test/bettery3`;
 
 export {
     path
