bf266f99860ed35fa3c38a309efe636e28340490
Andrii <asohrin9996@gmail.com>
2021-09-21 11:32:21 +0300

    added expertReputation.ts, userPayment fixed

--- contract-services/usersPayment.ts
+++ contract-services/usersPayment.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import {path} from "../config/path";
 import Web3 from "web3";
+import {expReputationCalc} from "./expertReputation"
 
 const usersPayment = async (data: any) => {
-
-    let {eventId, isMinted, dataForPayments} = getNeededData(data)
+    let {eventId, isMinted, dataForPayments, correctAnswer} = getNeededData(data)
 
     if (eventId) {
         let params = {
@@ -13,15 +13,14 @@ const usersPayment = async (data: any) => {
                 "publicEvents/validatorsAnswer",
                 {
                     "publicEvents/validatorsAnswer":
-                        ["publicActivites/from", {"publicActivites/from": ["users/wallet"]}]
+                        ["publicActivites/from", {"publicActivites/from": ["users/wallet", "users/expertReputPoins"]}, "publicActivites/answer"]
                 },
-                "publicEvents/parcipiantsAnswer", {"publicEvents/parcipiantsAnswer": ["publicActivites/amount", "publicActivites/from", {"publicActivites/from": ["users/wallet"]}]}
+                "publicEvents/parcipiantsAnswer", {"publicEvents/parcipiantsAnswer": ["publicActivites/amount", "publicActivites/from", {"publicActivites/from": ["users/wallet"]}, "publicActivites/answer"]}
 
             ],
-            "from": Number(eventId) //! 422212465065992
+            "from":Number(eventId)
         }
 
-        console.log(eventId, 'eventId')
         let dataFromDb: any = await axios.post(`${path}/query`, params).catch(err => {
             console.error("DB error in 'usersPayment': " + err.response.data.message)
             return
@@ -35,45 +34,49 @@ const usersPayment = async (data: any) => {
 
         let dataForSend: any;
 
-        const partcPayment = testSending(participants, dataForPayments, eventId, hostWallet, isMinted)
-        const validatorsPayment = testSending(validators, dataForPayments, eventId, hostWallet, isMinted)
+        const partcPayment = packingForSending(participants, dataForPayments, eventId, hostWallet, isMinted, correctAnswer)
+        const validatorsPayment = packingForSending(validators, dataForPayments, eventId, hostWallet, isMinted, correctAnswer)
 
         dataForSend = partcPayment.concat(validatorsPayment)
 
         const {isPartc, isValidators} = checkHost(participants, validators, hostWallet)
 
         if (!isPartc.length && !isValidators.length) {
+            let web3 = new Web3();
             let hostData = dataForPayments.filter((o: any) => {
                 return o[0].key == 'recipient' && o[0].value == hostWallet
             })
-            let web3 = new Web3();
-            dataForSend.push({
-                '_id': Number(eventId),
-                'payHostAmount': web3.utils.fromWei(String(hostData[0][2].value.replace(/[a-zа-яё]/gi, '')), 'ether'),
-            })
+
+            if(isMinted == 'false') {
+                dataForSend.push({
+                    '_id': Number(eventId),
+                    'payHostAmount': web3.utils.fromWei(String(hostData[0][2].value.replace(/[a-zа-яё]/gi, '')), 'ether'),
+                })
+            }
+            if(isMinted == 'true'){
+                dataForSend.push({
+                    '_id': Number(eventId),
+                    'payHostAmount': web3.utils.fromWei(String(hostData[1][2].value.replace(/[a-zа-яё]/gi, '')), 'ether'),
+                    'mintedHostAmount': web3.utils.fromWei(String(hostData[0][2].value.replace(/[a-zа-яё]/gi, '')), 'ether'),
+                })
+            }
         }
-        sendToDB(dataForSend)
+
+        const expRepSend = expReputationCalc(validators, Number(correctAnswer))
+        sendToDB(dataForSend.concat(expRepSend.forSendReputation))
     }
 
 
 }
 
-const findQuantity = (arr: any, wallet: string) => {
-    let all = arr.filter((el: any) => {
-        return el[0].key == 'recipient' && el[0].value == wallet
-    })
-    return all.length
-}
-
-const testSending = (partc: any, data: any, eventId: any, hostWallet: string, isMinted: string) => {
+const packingForSending = (partc: any, data: any, eventId: any, hostWallet: string, isMinted: string, correctAnswer: number) => {
     let web3 = new Web3();
     let arr: any = []
-    // todo sort only winner
+    // todo ? sort only winner
     partc.map((el: any) => {
         let wallet = el['publicActivites/from']['users/wallet']
-        let amount = el['publicActivites/amount']
+        let answer = el['publicActivites/answer']
 
-        let howMany = findQuantity(data, wallet)
         let sortData = data.filter((o: any) => {
             if (o[0].key == 'recipient' && o[0].value == wallet) {
                 return o
@@ -81,41 +84,53 @@ const testSending = (partc: any, data: any, eventId: any, hostWallet: string, is
         })
 
         if (wallet == hostWallet) {
-            let pay1: any, pay0: any
+            let pay2: any = undefined, pay1: any, pay0: any
             let minted: any = undefined
 
-
             if (isMinted == 'true') {
-                console.log('isMinted true')
-                // pay1 = sortData[1] == undefined ? undefined : web3.utils.fromWei(String(sortData[2][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
-                // pay0 = sortData[0] == undefined ? undefined : web3.utils.fromWei(String(sortData[1][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
-                // minted = sortData[0] == undefined ? undefined : web3.utils.fromWei(String(sortData[0][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
+                pay1 = sortData[2] == undefined ? undefined : web3.utils.fromWei(String(sortData[2][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
+                pay0 = sortData[1] == undefined ? undefined : web3.utils.fromWei(String(sortData[1][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
+                minted = sortData[0] == undefined ? undefined : web3.utils.fromWei(String(sortData[0][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
+
+                if(Number(correctAnswer) === Number(answer)) {
+                    pay2 = sortData[3] == undefined ? undefined : web3.utils.fromWei(String(sortData[3][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
+                }
+
+                arr.push({
+                        '_id': Number(eventId),
+                        'payHostAmount': pay0,
+                        'mintedHostAmount': minted
+                    },
+                    {
+                        '_id': Number(el._id),
+                        "mintedToken": pay1,
+                        "payToken": pay2 ? pay2 : 0,
+                        "premiumToken": 0 //todo premiumToken
+                    }
+                )
             }
             if (isMinted == 'false') {
-                console.log('isMinted false')
                 pay1 = sortData[1] == undefined ? undefined : web3.utils.fromWei(String(sortData[1][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
                 pay0 = sortData[0] == undefined ? undefined : web3.utils.fromWei(String(sortData[0][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
-            }
 
 
-            if (pay1 == undefined) {
-                arr.push({
-                    '_id': Number(eventId),
-                    'payHostAmount': pay0
-                })
-            }
-            if (pay1 !== undefined && pay0 !== undefined) {
-                let payToken = pay1
-                let payHost = pay0
-                arr.push({
-                    '_id': Number(el._id),
-                    "mintedToken": 0,
-                    "payToken": payToken,
-                    "premiumToken": 0 //todo premiumToken
-                }, {
-                    '_id': Number(eventId),
-                    'payHostAmount': payHost
-                })
+                if (pay1 == undefined) {
+                    arr.push({
+                        '_id': Number(eventId),
+                        'payHostAmount': pay0
+                    })
+                }
+                if (pay1 !== undefined && pay0 !== undefined) {
+                    arr.push({
+                        '_id': Number(el._id),
+                        "mintedToken": 0,
+                        "payToken": pay1,
+                        "premiumToken": 0 //todo premiumToken
+                    }, {
+                        '_id': Number(eventId),
+                        'payHostAmount': pay0,
+                    })
+                }
             }
         }
 
@@ -125,9 +140,9 @@ const testSending = (partc: any, data: any, eventId: any, hostWallet: string, is
                 pay1 = sortData[0] == undefined ? 0 : web3.utils.fromWei(String(sortData[0][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
                 minted = false
             }
-            if(isMinted == 'true'){
+            if (isMinted == 'true') {
                 minted = sortData[0] == undefined ? 0 : web3.utils.fromWei(String(sortData[0][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
-                pay1 = sortData[0] == undefined ? 0 : web3.utils.fromWei(String(sortData[1][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
+                pay1 = sortData[1] == undefined ? 0 : web3.utils.fromWei(String(sortData[1][2].value.replace(/[a-zа-яё]/gi, '')), 'ether');
             }
             arr.push({
                 '_id': Number(el._id),
@@ -157,18 +172,22 @@ const getNeededData = (data: any) => {
         return x.type == "transfer"
     })["attributes"]
 
+    let correctAnswer = eventDetails["attributes"].find((x: any) => {
+        return x['key'] == 'answerIndex'
+    })['value']
+
     return {
         eventId,
         isMinted,
-        dataForPayments
+        dataForPayments,
+        correctAnswer
     }
 }
 
 const sendToDB = (params: any) => {
-    console.log(params, 'dataForSend')
-    // axios.post(path + '/transact', params).catch(err => {
-    //     console.log('error in usersPayment: ' + err.response.statusText)
-    // })
+    axios.post(path + '/transact', params).catch(err => {
+        console.log('error in usersPayment: ' + err.response.statusText)
+    })
     console.log("usersPayment: data sent successfully")
 }
 
