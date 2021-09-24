cfa4a36b662d964721a3c5c9585f5e7d630f1850
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-09-24 13:49:34 +0300

    refactoring

---
+++ contract-services/publicEvents/index.ts
@@ -0,0 +1,11 @@
+import { findCorrectAnswer } from "./findCorrectAnswer";
+import { payToRefferers } from "./payToRefferers";
+import { reverted } from "./reverted";
+import { expertCalc } from "./exprestCalc";
+
+export {
+    findCorrectAnswer,
+    payToRefferers,
+    reverted,
+    expertCalc
+}
