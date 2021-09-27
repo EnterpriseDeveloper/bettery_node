8431f83257bf0a6812dd4b5baf7a5cc0ed671873
ANDREW.KOZYAR <andrew.kozyar@gmail.com>
2021-09-27 10:33:56 +0300

    fix install bug

--- build_scripts/start.sh
+++ build_scripts/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo docker stop bettery_app
-sudo docker stop app_redis_1
-sudo docker rm bettery_app
-sudo docker rm app_redis_1
-sudo docker rmi app_app
-
 cp -r /home/ubuntu/node/keys /home/ubuntu/app/
 cd /home/ubuntu/app/
 sudo docker-compose up -d
