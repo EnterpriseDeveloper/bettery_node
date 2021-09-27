431c3a66bff4dee500365585a8e743d04b08adb6
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-09-27 23:20:23 +0300

    remove trash

--- config/path.ts
+++ config/path.ts
@@ -2,8 +2,7 @@ import { APIprod, APItest, demon, testDemon } from "./key";
 
 const ip = process.env.NODE_ENV == "production" ? APIprod : APItest;
 const demonPath = process.env.NODE_ENV == "production" ? demon : testDemon;
-// const path = `http://${ip}/fdb/demo/quize`;
-const path = `http://localhost:8090/fdb/test/bettery2`;
+const path = `http://${ip}/fdb/demo/quize`;
 const demonAPI = `http://${demonPath}`
 
 export {
