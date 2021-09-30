0def13f219eab8a2073b7265ccf3d91686a0a59d
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-09-30 20:08:09 +0300

    bad idea

--- index.ts
+++ index.ts
@@ -1,12 +1,3 @@
-var cluster = require('cluster');
-if (cluster.isMaster) {
-    cluster.fork();
-
-    cluster.on('exit', function (worker: any, code: any, signal: any) {
-        cluster.fork();
-    });
-}
-
 import express from 'express';
 import bodyParser from 'body-parser'
 const app = express();
@@ -32,57 +23,55 @@ Image(app);
 import TokenSale from './contract-services/tokensale';
 TokenSale(app);
 
-if (cluster.isWorker) {
+if (process.env.NODE_TEST == 'false') {
 
-    if (process.env.NODE_TEST == 'false') {
+    let key = "./keys/key.pem",
+        cert = "./keys/star_bettery_io.crt",
+        ca1 = "./keys/DigiCertCA.crt",
+        ca2 = "./keys/My_CA_Bundle.crt",
+        ca3 = "./keys/TrustedRoot.crt";
 
-        let key = "./keys/key.pem",
-            cert = "./keys/star_bettery_io.crt",
-            ca1 = "./keys/DigiCertCA.crt",
-            ca2 = "./keys/My_CA_Bundle.crt",
-            ca3 = "./keys/TrustedRoot.crt";
+    var credentials = {
+        key: fs.readFileSync(key, 'utf8'),
+        cert: fs.readFileSync(cert, 'utf8'),
+        ca: [
+            fs.readFileSync(ca1, 'utf8'),
+            fs.readFileSync(ca2, 'utf8'),
+            fs.readFileSync(ca3, 'utf8')
+        ]
+    };
 
-        var credentials = {
-            key: fs.readFileSync(key, 'utf8'),
-            cert: fs.readFileSync(cert, 'utf8'),
-            ca: [
-                fs.readFileSync(ca1, 'utf8'),
-                fs.readFileSync(ca2, 'utf8'),
-                fs.readFileSync(ca3, 'utf8')
-            ]
-        };
-
-        var httpsServer = https.createServer(credentials, app);
-        var io = require('socket.io')(httpsServer);
-        comments(io);
-        httpsServer.listen(443);
-    }
-
-
-    var cors = require('cors');
-
-    app.use(cors({
-        origin: "*"
-    }))
-    app.use(bodyParser.json({ limit: '50mb' }));
-    app.use(bodyParser.text({ limit: '50mb' }));
-    app.use(bodyParser.urlencoded({
-        limit: '50mb',
-        extended: true
-    }));
-    var httpServer = http.createServer(app);
-
-    if (process.env.NODE_TEST != 'false') {
-        var io = require('socket.io')(httpServer);
-        comments(io);
-    }
-
-    httpServer.listen(80, async () => {
-        await loadHandler();
-        setInterval(() => {
-            refundBot();
-        }, 1000 * 60 * 60 * 24 * 3);
-
-        console.log("server run port 80");
-    });
+    var httpsServer = https.createServer(credentials, app);
+    var io = require('socket.io')(httpsServer);
+    comments(io);
+    httpsServer.listen(443);
 }
+
+
+var cors = require('cors');
+
+app.use(cors({
+    origin: "*"
+}))
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.text({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+}));
+var httpServer = http.createServer(app);
+
+if (process.env.NODE_TEST != 'false') {
+    var io = require('socket.io')(httpServer);
+    comments(io);
+}
+
+httpServer.listen(80, async () => {
+    await loadHandler();
+    setInterval(() => {
+        refundBot();
+    }, 1000 * 60 * 60 * 24 * 3);
+
+    console.log("server run port 80");
+});
+
