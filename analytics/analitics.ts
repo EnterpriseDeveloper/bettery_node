3d72a8fb8b67bc98540446b2feabd2ea590108df
Andrii <asohrin9996@gmail.com>
2021-10-14 15:41:41 +0300

    2th

--- analytics/analitics.ts
+++ analytics/analitics.ts
@@ -2,31 +2,122 @@ import axios from "axios";
 import {path} from "../config/path";
 
 const analytics24h = async (req: any, res: any) => {
-    let time = new Date().setHours(0,0,0,0) / 1000
-    let  day = 86400
+    let time = new Date().setHours(0, 0, 0, 0) / 1000
+    let day = 86400
 
     let params: any = {
-        "select": ["*"],
-        "where": `publicEvents/dateCreation > ${time - day} AND publicEvents/dateCreation < ${time}`
+        "select": ["publicEvents/dateCreation", {
+            "publicEvents/parcipiantsAnswer": [
+                {"publicActivites/from": ["users/isBot"]}
+            ]
+        }, {
+            "publicEvents/validatorsAnswer": [
+                {"publicActivites/from": ["users/isBot"]}
+            ]
+        }],
+        "where": `publicEvents/dateCreation > ${time - day}`
     }
 
-    let data = axios.get(`${path}/query`, params).catch((err)=> {
+    let data: any = await axios.post(`${path}/query`, params).catch((err) => {
         res.status(400)
         res.send(`Error from DB: ${err.response.statusText}`)
         return
     })
 
+    if (data.data.length) {
+        let pureData = data.data.filter((el: any) => {
+            return el['publicEvents/dateCreation'] < time
+        })
 
-    res.status(200)
-    res.send({done: "OK"})
+        let {parc, val} = await letsFindRealUsers(pureData)
+
+        res.status(200)
+        res.send({
+            eventAmount: pureData.length,
+            parcipiants: parc.length,
+            validators: val.length
+        })
+    }
+}
+
+let letsFindRealUsers = async (pureData: any) => {
+    let parc: any = []
+    let val: any = []
+
+    for (let i = 0; i < pureData.length; i++) {
+        if (pureData[i]['publicEvents/parcipiantsAnswer'] != undefined) {
+            pureData[i]['publicEvents/parcipiantsAnswer'].map((el: any) => {
+
+                if (!el['publicActivites/from']['users/isBot']) {
+                    parc.push(el['publicActivites/from']._id)
+                }
+            })
+        }
+        if (pureData[i]['publicEvents/validatorsAnswer'] != undefined) {
+            pureData[i]['publicEvents/validatorsAnswer'].map((el: any) => {
+
+                if (!el['publicActivites/from']['users/isBot']) {
+                    val.push(el['publicActivites/from']._id)
+                }
+            })
+        }
+    }
+    parc = unique(parc)
+    val = unique(val)
+
+    return {parc, val}
+}
+
+let unique = (arr: any) => {
+    let result: any = [];
+
+    for (let str of arr) {
+        if (!result.includes(str)) {
+            result.push(str);
+        }
+    }
+    return result;
 }
 
 const analyticsByEventId = async (req: any, res: any) => {
+    let eventId = req.body.id
+
+    let params = {
+        "select": ["publicEvents/status", {
+            "publicEvents/parcipiantsAnswer": [
+                {"publicActivites/from": ["users/isBot"]}
+            ]
+        }, {
+            "publicEvents/validatorsAnswer": [
+                {"publicActivites/from": ["users/isBot"]}
+            ]
+        }],
+        "from": eventId
+    }
+
+    let data: any = await axios.post(`${path}/query`, params).catch((err: any) => {
+        res.status(400)
+        res.send(`Error from DB: ${err.response.statusText}`)
+        return
+    })
+
+    let event = data.data
+    let {parc, val} = await letsFindRealUsers(event)
+
+
     res.status(200)
-    res.send({done: "OK"})
+    res.send({
+        finished: !!event[0]['publicEvents/finishTime'],
+        status: event[0]['publicEvents/status'],
+        parcipiants: parc.length,
+        validators: val.length
+    })
 }
 
-export  {
+
+
+
+export {
     analytics24h,
     analyticsByEventId
 }
--- analytics/index.ts
+++ analytics/index.ts
@@ -2,11 +2,11 @@ import { analytics24h, analyticsByEventId } from "./analitics";
 
 export default function Analytics(app: any) {
 
-   app.post('/analytics_ById', async (req: any, res: any) => {
+   app.get('/full_analytics', async (req: any, res: any) => {
        await analytics24h(req, res)
    })
 
-    app.get('/full_analytics', async (req: any, res: any) => {
+    app.post('/analytics_ById', async (req: any, res: any) => {
         await analyticsByEventId(req, res)
     })
 }
